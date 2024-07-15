53a37a7615a5b6282b4e069de105cd5fd2666706
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-15 15:55:26 -0300

    Creating a count text application

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonTextarea } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,8 +9,31 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonItem, 
+    IonLabel, 
+    IonTextarea, 
+    IonList, 
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    FormsModule,
+    ExploreContainerComponent
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Tab1Page {
+  text: string = '';
+  caracters: number = 0;
+  words: number = 0;
+  lines: number = 0;
+
   constructor() {}
+
+  countText() {
+    this.caracters = this.text.length;
+    this.words = this.text.trim().split(/\s+/).filter((word) => word.length > 0).length;
+    this.lines = this.text.split('\n').length;
+  }
 }
